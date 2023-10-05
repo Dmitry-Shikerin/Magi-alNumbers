fff229caaa3dc5a2b6af87a55c5f555ed053fc8d
Dmitry-Shikerin <130661730+Dmitry-Shikerin@users.noreply.github.com>
2023-10-05 15:07:08 +0300

    Добавьте файлы проекта.

---
+++ Магические числа/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Магические_числа
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Weapon
+    {
+        private const int _minValueBullets = 0;
+        private const int _bulletsPerShot = 1;
+
+        private int _bullets;
+
+        public bool CanShoot() => _bullets > _minValueBullets;
+
+        public void Shoot() => _bullets -= _bulletsPerShot;
+    }
+}
